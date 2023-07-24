a9101a548e67ce8b2dc85cf58bfe15643b451e92
Djotchou20 <djotchou20@gmail.com>
2023-07-24 15:43:59 +0100

    first Api commit

---
+++ lib/LoginPage.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'main.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController secretController = TextEditingController();
+  final String apiUrl =
+      'https://testenv.ciphernet.net/ngcomintranetv2/api/v1/auth/getkeys';
+  final String apiKey = 'k840o8ccsw8cswkos084g44w4cgco0ccgkcc08w0';
+
+  @override
+  void initState() {
+    super.initState();
+    checkSavedCredentials();
+  }
+
+  void checkSavedCredentials() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String savedUsername = prefs.getString('username') ?? '';
+    String savedSecret = prefs.getString('secret') ?? '';
+
+    if (savedUsername.isNotEmpty && savedSecret.isNotEmpty) {
+      authenticateUser(savedUsername, savedSecret);
+    }
+  }
+
+  Future<void> authenticateUser(String username, String secret) async {
+    try {
+      final http.Response response = await http.post(
+        Uri.parse(apiUrl),
+        body: {
+          'username': username,
+          'secret': secret,
+        },
+      );
+
+      final Map<String, dynamic> responseData = jsonDecode(response.body);
+      final String _apikey = responseData['api_key'];
+
+      print('Response Status Code: ${response.statusCode}');
+      print('Response Body: ${response.body}');
+
+      if (response.statusCode == 200 && _apikey.isNotEmpty) {
+        // Save the API key using shared preferences.
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        prefs.setString('api_key', _apikey);
+
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => MyHomePage()),
+        );
+      } else {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Authentication Failed'),
+            content: Text('Invalid credentials. Please try again.'),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: Text('OK'),
+              ),
+            ],
+          ),
+        );
+      }
+    } catch (error) {
+      print(error);
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Error'),
+          content: Text('Incorrect Username or Password.'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text('OK'),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: usernameController,
+                decoration: InputDecoration(
+                  hintText: 'Username',
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: secretController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                ),
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  authenticateUser(
+                    usernameController.text,
+                    secretController.text,
+                  );
+                },
+                child: Text('Login'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,152 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'LoginPage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'SiteSurvey',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        routes: {
+          '/': (context) => LoginPage(),
+          '/details': (context) => Details(item: []),
+        });
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<dynamic> data = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    const String apiKey = 'gw8c4kos0wgsgogw408o88wwwwk4cs80s80g480g';
+    const String apiUrl =
+        'https://testenv.ciphernet.net/ngcomintranetv2/api/v1/sitesurveyreq/getpendingsitesurveysbyuser?username=admin'; // Replace with your API URL
+
+    final headers = {
+      'x-api-key': apiKey,
+    };
+
+    try {
+      final http.Response response =
+          await http.get(Uri.parse(apiUrl), headers: headers);
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> responseData = jsonDecode(response.body);
+        final List<dynamic> fetchData = responseData['data'];
+        setState(() {
+          data = fetchData;
+        });
+        print(data);
+      } else {
+        throw Exception(
+            'Failed to fetch data from API. Status Code: ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Error: $error');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SITE SURVEY'),
+      ),
+      body: ListView.builder(
+        itemCount: data.length,
+        itemBuilder: (BuildContext context, int index) {
+          final item = data[index];
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => Details(item: data[index])),
+              );
+            },
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Client ID: ${item['id'] ?? ''}'),
+                    Text('Clientname: ${item['clientname'] ?? ''}'),
+                    Text('Email: ${item['email'] ?? ''}'),
+                    Text('Phone Number: ${item['phone'] ?? ''}'),
+                    Text('Site Address: ${item['siteaddress'] ?? ''}'),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class Details extends StatelessWidget {
+  final dynamic item;
+
+  Details({required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Details'),
+      ),
+      body: ListView(
+        shrinkWrap: true,
+        padding: const EdgeInsets.all(8.0),
+        children: [
+          SizedBox(height: 10),
+          Text('Client ID: ${item['id'] ?? ''}'),
+          SizedBox(height: 10),
+          Text('Client Name: ${item['clientname'] ?? ''}'),
+          SizedBox(height: 10),
+          Text('Client Email: ${item['email'] ?? ''}'),
+          SizedBox(height: 10),
+          Text('Client Phone: ${item['phone'] ?? ''}'),
+          SizedBox(height: 10),
+          Text('Client Site Address: ${item['siteaddress'] ?? ''}'),
+          SizedBox(height: 10),
+          Text('Client Coordinate: ${item['clientcoordinate'] ?? ''}'),
+          SizedBox(height: 10),
+          Text('Client Distance: ${item['distance'] ?? ''}'),
+          SizedBox(height: 10),
+          Text('Client SiteSurvey Number: ${item['sitesurveynumber'] ?? ''}'),
+          SizedBox(height: 10),
+          Text('Client SiteSurvey Type: ${item['site_survey_type'] ?? ''}'),
+          SizedBox(height: 10),
+          Text('Client Is_Fibre: ${item['is_fibre'] ?? ''}'),
+        ],
+      ),
+    );
+  }
+}
